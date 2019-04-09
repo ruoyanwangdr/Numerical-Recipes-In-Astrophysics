fd05585126bda0b58972d82a7dfd51682ad30444
rywjhzd <rywang@strw.leidenuniv.nl>
2019-04-09 14:12:15 +0200

    Update trilinear_interp.py

--- Hand_in_exercise_1/trilinear_interp.py
+++ Hand_in_exercise_1/trilinear_interp.py
@@ -25,7 +25,16 @@ def trilinear_interpolation(vertex, index, a, b, c):
     vertex   ...  3d arrays wanted to interpolate
     index       ...  indices at which the vertex will be evaluated.
 
-    Result of interpolation if choosing x,y,z = 14, 15, 25,
+    Find the eight corners of a cube that surround our point of interest.
+    Then perform interpolation between the upper and lower edges of front and back sides.
+    This will give us interplolations of four lines so that we can find four points along
+    them. Repeat the above process between the interpolation of the upper lower points found
+    by the previous interpolation. This will return two points of the top and bottom sides.
+    Perform the interpolation the third time and this will give the desired point within the boundary.
+    The product of the value at the desired point and the entire volume is equal to the sum
+    of the products of the value at each corner and the partial volume diagonally opposite the corner.
+
+    Here I give the result of my interpolation if choosing x,y,z = 14, 15, 25,
     in comparison with the output of new_A:
 
     >>> new_A[14][15][25]
@@ -46,18 +55,20 @@ def trilinear_interpolation(vertex, index, a, b, c):
     y0 = np.array([index[1]])
     z0 = np.array([index[2]])
 
-    # lattice points above x,y,z.
+    # lattice points above x,y,z. For example, if we treat the origin (0,0,0) as (x0,y0,z0),
+    # then the vertex (1,0,0) will just be (x1,x0,x0), and so on.
     x1 = x0 + 1
     y1 = y0 + 1
     z1 = z0 + 1
 
-    # Make sure vertices are within the index boundary.
+    # Since we are counting the index, vertices are confined within the index boundary.
     x1[np.where(x1==vertex.shape[0])] = x0.max()
     y1[np.where(y1==vertex.shape[1])] = y0.max()
     z1[np.where(z1==vertex.shape[2])] = z0.max()
 
-    # Dimensions of the box.
-    # Default is the unit cube with a=b=c=0. This will return A unweighted.
+    # Dimensions of the inner box. Here I define a small box inside the bigger boundary.
+    # This will be our point of interest in 3D.
+    # Default is the unit cube with a=b=c=0. It will return A unweighted.
     x = np.array([a])
     y = np.array([b])
     z = np.array([c])
